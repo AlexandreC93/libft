7fe77b610b70ed126442d324ecaf99ce80d9f262
AlexandreC93 <alexandrecadinot93@gmail.com>
2022-11-08 18:57:21 +0100

    added

---
+++ ft_striteri.c
@@ -0,0 +1,9 @@
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+
+}
+
+int	main(int argc, char **argv)
+{
+	return (0);
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,12 @@
+int	ft_test(void)
+{
+	
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+
+int	main(int argc, char **argv)
+{
+	printf("%s", ft_strmapi(argv[1], ft_test()));
+	return (0);
+}
