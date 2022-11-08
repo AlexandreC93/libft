f3b011b42c261e51531371055f63f7d6c14ca6aa
AlexandreC93 <alexandrecadinot93@gmail.com>
2022-11-08 20:05:05 +0100

    added

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,12 +1,19 @@
-int	ft_test(void)
-{
-	
-}
-
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int	i;
+	char	*dest;
+
+	dest = ft_strdup(s);
+	if (!dest)
+	i = 0;
+	while (s[i])
+		d = (*f)(i++, s[i]);
+	return (d);
+
+}
 
 int	main(int argc, char **argv)
 {
-	printf("%s", ft_strmapi(argv[1], ft_test()));
+	printf("%s", ft_strmapi(argv[1], ft_test(argv[1], argv[2])));
 	return (0);
 }
